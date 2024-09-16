96c3c21b7d061cb00af790eb9e7300e434513ee2
Christopher Blessing <74223087+Spellpunk101@users.noreply.github.com>
2024-09-15 20:46:32 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -150,7 +150,7 @@ function addContact()
     let newEmail = document.getElementById("emailText").value;
     //document.getElementById("contactAddResult").innerHTML = "";
 
-    let tmp = {name:newName, phone:newPhone, email:newEmail, userID:userID};
+    let tmp = {name:newName, phone:newPhone, email:newEmail, userId:userId};
     let jsonPayload = JSON.stringify( tmp );
 
     let url = urlBase + '/AddContact.' + extension;
